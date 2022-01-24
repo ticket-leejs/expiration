33446892ee728a18ce8bc7760611e011974d5a56
leejunsu <75758585@naver.com>
2022-01-24 21:24:38 +0900

    add console for test cicd

--- src/nats-wrapper.ts
+++ src/nats-wrapper.ts
@@ -17,6 +17,7 @@ class NatsWrapper {
     return new Promise((resolve, reject) => {
       this.client.on('connect', () => {
         console.log('Connected to NATS');
+        console.log('test jenkins');
         resolve();
       });
       this.client.on('error', (err) => {
